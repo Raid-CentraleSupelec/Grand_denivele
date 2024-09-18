16e98be5d1f2b33469b6f0e51cbc6fe2451cea3f
Houillier Thomas <thomas.houillier@student-cs.fr>
2024-09-18 19:07:43 +0200

    Enfin

--- affichage_grand_denivele/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import equipe
-
-admin.site.register(equipe)
-
-class EquipeAdmin(admin.ModelAdmin):
-    list_display = ('equipe', 'altitude')
-    search_fields = ('equipe',)
--- affichage_grand_denivele/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AffichageGrandDeniveleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'affichage_grand_denivele'
